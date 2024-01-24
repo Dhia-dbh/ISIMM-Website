33c77e8e1bf0ec6ca2e0b0aa5d46e73faf48ed04
Ahmed5827 <ssjzchebbi@gmail.com>
2024-01-24 22:28:56 +0100

    adding footer to the profile

--- src/components/Profile_etudiant/Profile_etudiant.jsx
+++ src/components/Profile_etudiant/Profile_etudiant.jsx
@@ -3,8 +3,10 @@ import './Profile_etudiant.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-
+import { FaFacebook } from "react-icons/fa";
 import isimm1 from "../../assets/pfp.jpg";
+import NavBar2 from '../commun/navbar/NavBar2';
+import { BrowserRouter } from 'react-router-dom';
 function Profile_etudiant (){
     const [Nom, setNom] = useState("Ahmed")
     const [Prenom, setPrenom] = useState("Chebbi")
@@ -19,6 +21,8 @@ function Profile_etudiant (){
       };
     return(
         <div id='Page'>
+       
+        <NavBar2></NavBar2>
         <div id='Header'>
             <div id='pfp'>
                 <img  src={isimm1} alt='ISIMM' width="150" height="150" id='photo_profile' ></img>
@@ -36,7 +40,9 @@ function Profile_etudiant (){
             <button type='button' id='but' onClick={Contact} class="btn btn-primary" >Modifier</button>
             </div>
         </div>
+        <hr  id='sep' ></hr>
         <div id='Information' >
+        
         <div id='Grades'>
             <h3>Grades</h3>
             <table>
@@ -77,7 +83,17 @@ function Profile_etudiant (){
                 </tr>
             </table>
         </div>
+        
         </div>
+        
+        <footer id='foot'>
+            <p className='Ifoot'>Conception, réalisation :<br></br> Ing.Ahmed Chebbi<br></br> Ing.Dhia ben hammouda</p>
+            <p  className='Ifoot'>Téléphone :+216 70 011 920 <br></br>Fax : +216 70 011 959</p>
+            <p  className='Ifoot'>Adress:<br></br> Avenue de la Corniche, Monastir 5000 Tunisie</p>
+            <p className='Ifoot'>Connect With Us <br/><FaGithub id='git' size={23} /> <FaFacebook id='facebook' size={23} /> <FaLinkedin id='linkedin' size={23}/></p>
+            
+        </footer>
+        
         </div>
     )
 
