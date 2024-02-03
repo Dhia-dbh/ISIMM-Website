e83a415db335f67dbe343d3de6849fa336d0437d
Dhia-dbh <benhamoudadhia@isimm.u-monastir.tn>
2024-02-03 01:48:10 +0100

    Added Actualite and clubs sections

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,6 +5,8 @@ import NavBar from "../commun/navbar/NavBar";
 import "./home.css";
 
 import isimm1 from "../../assets/isimm1.jpg";
+import pfp from "../../assets/pfp.jpg";
+import clubsbg from "../../assets/bge.jpg";
 
 const vieEstudiantineSubmenu = [
   { name: "Profile", path: "/etudiants/profile" },
@@ -61,13 +63,34 @@ const Home = () => {
         <h2 className="title mt-5 mb-5">Actualité</h2>
         <div className="actualiteHolder">
           {actualiteItems.map((item, index) => (
-            <div key={index} className="actualiteItem">
-              <h4>{item.title}</h4>
-              <div className="actualiteDescription">
-                <p>{item.description} lorem</p>
+            <div className="actualiteContainer">
+              <div className="actualiteImg">
+                <img src={pfp} alt="Intrests" />
+              </div>
+              <div key={index} className="actualiteItem">
+                <h4>{item.title}</h4>
+                <div className="actualiteDescription">
+                  <p>{item.description} lorem</p>
+                </div>
               </div>
             </div>
           ))}
+        </div>
+        <div className="clubsHolder">
+          <section className="clubsDescriptionSection">
+            <h2 className="title">Clubs</h2>
+            <div className="clubsDescription">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
+              vero atque voluptates distinctio dolorum deleniti nulla unde
+              obcaecati error recusandae numquam rerum! Facilis iste veniam
+              maiores totam inventore doloremque reprehenderit.
+            </div>
+          </section>
+          <section className="clubsLogoSection">
+            <div className="clubsImg">
+              <img src={pfp} alt="Clubs Icon" />
+            </div>
+          </section>
         </div>
       </section>
     </>
