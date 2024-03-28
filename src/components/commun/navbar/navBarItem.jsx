6c58a21e034f8138ce7a226034a8ab2fc5f2839b
Dhia-dbh <benhamoudadhia@isimm.u-monastir.tn>
2024-03-28 04:12:15 +0100

    NavBar item with current path is disabled

--- src/components/commun/navbar/navBarItem.jsx
+++ src/components/commun/navbar/navBarItem.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 export default function NavBarItem({ _key, isScrolled, item }) {
+  const location = useLocation();
+  const currentPath = location.pathname; // Get the entire path after domain
+
   return (
     <a
       key={_key}
       className={isScrolled ? "isScrolledText" : "notScrolledText"}
       href={item.path}
+      onClick={(e) => {
+        if (currentPath === item.path) {
+          e.preventDefault();
+        }
+      }}
     >
       {item.name}{" "}
     </a>
